d0563c64b126edd0f8452d77a61b72d17a4883ba
Sara Echeverría <84192821+bl33h@users.noreply.github.com>
2022-04-27 07:37:41 -0600

    initial commit.

---
+++ Cadenas.s
@@ -0,0 +1,12 @@
+/* -----------------------------------------------
+* UNIVERSIDAD DEL VALLE DE GUATEMALA
+* Sara Maria Perez Echeverria 21371
+* Fabian Estuardo Juarez Tello 21440
+* Organización de computadoras y Assembler
+* Ciclo 1 - 2022
+*
+* Programa que determina la compatibilidad del
+* primer nombre con el primer apellido y la buena
+* fortuna del individuo en base a un sistema de
+* puntaje basado en 3 diferentes criterios.
+ ----------------------------------------------- */
