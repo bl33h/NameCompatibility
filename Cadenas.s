f3f14c7164b241cc6238cfe60242d72305e3f65a
FabianJuarez182 <jua21440@uvg.edu.gt>
2022-04-27 07:57:35 -0600

    init program

--- Cadenas.s
+++ Cadenas.s
@@ -10,3 +10,38 @@
 * fortuna del individuo en base a un sistema de
 * puntaje basado en 3 diferentes criterios.
  ----------------------------------------------- */
+    @@ codigo de assembler: se coloca en la seccion .text
+.text
+.align 2
+    @@ etiqueta "main" llama a la funcion global
+.global main
+.type main, %function
+main:
+    @@ grabar registro de enlace en la pila
+stmfd sp!, {lr}
+/*impresion de menu y pide comando*/
+
+
+/* si pone q sale*/
+Salir:
+ldr r0,=adios
+ldr r1,
+bl puts
+mov r0, #0
+mov r3, #0
+ldmfd sp!, {lr}
+bx lr
+.data
+.align 2
+
+n: .asciz ""
+a: .asciz ""
+f: .word 0
+
+ingresoN:
+    .asciz "Ingrese su nombre: %s\n"
+ingresoA:
+    .asciz "Ingrese su apellido: %s\n"
+adios:
+    .asciz "Su fortuna tiene un puntaje de: %d"
+ 
